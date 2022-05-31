2aa5c56632c920a6b78e19cb106085be5d6fcca6
groupdevelopment-cinemaparadise <106568442+groupdevelopment-cinemaparadise@users.noreply.github.com>
2022-05-31 10:27:47 +0530

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -5,7 +5,7 @@ var baseURL;
 // if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
 //     baseURL = process.env.REACT_APP_API_BASE_URL;
 // } else {
-baseURL = 'https://backend-cinema.herokuapp.com/';
+baseURL = 'https://backend-cinemaapp.herokuapp.com/';
 // }
 
 const api = axios.create({
